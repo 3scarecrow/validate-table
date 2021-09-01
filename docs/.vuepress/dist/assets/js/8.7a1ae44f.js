329dc6603953e79eb884ac33b713f41c27cc91da
3sunjunhao <sunjunhao@banggood.com>
2021-09-01 14:42:12 +0800

    build: 生成文档

--- docs/.vuepress/dist/assets/js/8.7a1ae44f.js
+++
@@ -1,1 +0,0 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[8],{459:function(t,e,n){"use strict";n.r(e);var s={},l=n(41),a=Object(l.a)(s,(function(){var t=this.$createElement;return(this._self._c||t)("div",{staticClass:"demo-box"})}),[],!1,null,null,null);e.default=a.exports}}]);
